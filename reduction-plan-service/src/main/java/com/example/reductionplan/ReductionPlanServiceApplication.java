45325715e24815070f38d541d0465054617f0e61
MaryJml <meilai.ji@kcl.ac.uk>
2026-06-16 21:35:42 +0100

    initialize the project

---
+++ reduction-plan-service/src/main/java/com/example/reductionplan/ReductionPlanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reductionplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReductionPlanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReductionPlanServiceApplication.class, args);
+	}
+
+}
